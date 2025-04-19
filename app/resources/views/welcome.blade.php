4f17b4fd7d160083351decab1dc8d4409527ced1
Emmanuel <emmanuelayofame@gmail.com>
2025-04-19 20:44:34 +0500

    m

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Milestar</title>
+        <title>Milestar Trade And Export Limited</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
