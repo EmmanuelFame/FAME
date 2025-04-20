c4ffd48101b350e4c36f0fbf35e3cec5db3b37f5
Emmanuel <emmanuelayofame@gmail.com>
2025-04-20 18:55:23 +0500

    webp

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -116,19 +116,19 @@
         <div class="relative h-full overflow-hidden rounded-lg">
             <!-- Item 1 -->
             <div class="hidden h-full duration-700 ease-in-out" data-carousel-item="active">
-                <img src="{{ asset('images/wheaty.png') }}" alt="..." class="absolute top-0 left-0 object-cover object-center w-full h-full">
+                <img src="{{ asset('images/wheaty.webp') }}" alt="..." class="absolute top-0 left-0 object-cover object-center w-full h-full">
             </div>
             <!-- Item 2 -->
             <div class="hidden h-full duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('images/wheaty_port2.png') }}" alt="..." class="absolute top-0 left-0 object-cover object-center w-full h-full">
+                <img src="{{ asset('images/wheaty_port2.webp') }}" alt="..." class="absolute top-0 left-0 object-cover object-center w-full h-full">
             </div>
             <!-- Item 3 -->
             <div class="hidden h-full duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('images/wheaty_port3.png') }}" alt="..." class="absolute top-0 left-0 object-cover object-center w-full h-full">
+                <img src="{{ asset('images/wheaty_port3.webp') }}" alt="..." class="absolute top-0 left-0 object-cover object-center w-full h-full">
             </div>
             <!-- Item 4 -->
             <div class="hidden h-full duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('images/wheaty_port.png') }}" alt="..." class="absolute top-0 left-0 object-cover object-center w-full h-full">
+                <img src="{{ asset('images/wheaty_port.webp') }}" alt="..." class="absolute top-0 left-0 object-cover object-center w-full h-full">
             </div>
         </div>
 
