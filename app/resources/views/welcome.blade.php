d3b308dd51f4f04f13a188e1eb95bc75a6086f96
Emmanuel <emmanuelayofame@gmail.com>
2025-05-08 13:36:08 +0500

    favicon

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -6,6 +6,9 @@
 
         <title>Milestar Trade</title>
 
+        <!-- Favicon -->
+    <link rel="icon" type="image/jpeg" href="{{ asset('images/milestar_logo.jpg') }}">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
