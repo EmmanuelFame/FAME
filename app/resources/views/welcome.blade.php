853d824daa0e814608840c975635d1356741a2c7
Emmanuel <emmanuelayofame@gmail.com>
2025-05-28 02:06:16 +0500

    f

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -122,7 +122,7 @@
                                 <li>
                                     <a href="{{ route('login') }}"
                                        class="block px-3 py-2 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
-                                        Login
+                                        Sign in
                                     </a>
                                 </li>
                                 @if (Route::has('register'))
