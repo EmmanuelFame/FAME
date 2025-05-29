69562de9c5a28eeb4bdcf210e47b3922dd1108c3
Emmanuel <emmanuelayofame@gmail.com>
2025-05-30 01:14:17 +0500

    b

--- app/routes/web.php
+++ app/routes/web.php
@@ -30,12 +30,12 @@ Route::prefix('ru')->group(function () {
     Route::get('/dashboard', fn () => view('ru.dashboard'))->middleware(['auth', 'verified'])->name('ru.dashboard');
 
     Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('ru.profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('ru.profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('ru.profile.destroy');
-    });
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-    Route::post('/contact', [ContactController::class, 'submit'])->name('ru.contact.submit');
+Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
 });
 
 // Locale switch POST endpoint
